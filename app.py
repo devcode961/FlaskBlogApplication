93a59314a71d1e58c84a6bb46444270046a56d3b
devcode961 <devcode961@gmail.com>
2022-02-28 14:01:55 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Swarnim Bhardwaj',
+        'title': 'Data Structures And Algorithms',
+        'content': 'My first blog content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Pushkar Kumar',
+        'title': 'Analysis of Algorithms',
+        'content': 'My second blog content',
+        'date_posted': 'May 20, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts, title='Home Page')
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About')
+
+if __name__ == '__main__':
+    app.run()
