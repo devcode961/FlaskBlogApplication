42eba1f9aa6e1aa48a298f87ac3c199ed7049f9f
devcode961 <devcode961@gmail.com>
2022-03-01 18:04:16 +0100

    Added register and login functionality along with form validations

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7a55d69d681e46fb75bc21fc99707676'
 
 posts = [
     {
@@ -17,14 +20,40 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts, title='Home Page')
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html',title='About')
+    return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You are now logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login unsuccessful!!!.Please check email and password and try again', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run()
